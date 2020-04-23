714da14ff58b0cca6d55a2f7a4963da747644fce
Gayathri <gayathri.rvijayan@gmail.com>
2020-04-23 16:03:23 +0200

    feat: minor changes

--- commands/cmd/sync.go
+++ commands/cmd/sync.go
@@ -16,9 +16,12 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"if0/config"
+	"os"
+	"path/filepath"
 )
 
 // syncCmd represents the sync command
@@ -26,9 +29,23 @@ var syncCmd = &cobra.Command{
 	Use:   "sync",
 	Short: "sync with a remote git repository",
 	Long: `This command helps sync .if0 config files with a remote git repository.
+
 The remote git repository to be synced with should be set in the ~/.if0/if0.env file 
-as a value for the config variable REMOTE_STORAGE `,
+as a value for the config variable REMOTE_STORAGE.
+
+If the REMOTE_STORAGE is a SSH repository, please also include SSH_KEY_PATH config variable 
+with path to ssh key (.ppk) as the value`,
+
 	Run: func(cmd *cobra.Command, args []string) {
+		rootPath, _  := os.UserHomeDir()
+		if0Config       := filepath.Join(rootPath, ".if0", "if0.env")
+		if _, err := os.Stat(if0Config); os.IsNotExist(err) {
+			fmt.Println("if0.env does not exist, " +
+				"please first run `if0 config` to set up default config. " +
+				"And, include config variables necessary for remote synchronization")
+			return
+		}
+
 		err := config.RepoSync()
 		if err != nil {
 			log.Errorln("Error while syncing with remote repo: ", err)
