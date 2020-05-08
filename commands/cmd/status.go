e544c235570b2a21a41f09a77867cddc6ef0b4a6
Gayathri <gayathri.rvijayan@gmail.com>
2020-05-08 12:38:53 +0200

    feat: including code to check for local dependencies

---
+++ commands/cmd/status.go
@@ -0,0 +1,59 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"if0/status/dep"
+)
+
+const (
+	depArg = "dep"
+)
+// statusCmd represents the status command
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "To check the status of services in if0",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		// checking if the required arguments are present
+		if len(args) < 1 {
+			fmt.Println("Please provide valid arguments.")
+			fmt.Println("example command: `if0 status dep` to check " +
+				"for the status of dependencies")
+			return
+		}
+
+		if args[0] == depArg {
+			dep.CheckDependencies()
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// statusCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
