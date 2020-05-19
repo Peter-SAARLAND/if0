33f8b6c02cb3a43a2f9fda57ca0131711851d878
Gayathri <gayathri.rvijayan@gmail.com>
2020-05-19 19:09:50 +0200

    feat: environment plan

---
+++ environments/dockercmd/dash1plan.go
@@ -0,0 +1,87 @@
+package dockercmd
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"if0/common"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+const (
+	dash1Image = "registry.gitlab.com/peter.saarland/dash1"
+)
+
+// This function is used to start a dash1 container, and run `make plan` inside the container.
+// In dash1, make plan initializes the necessary Terraform provider modules for
+// the Environment 'envName' and then creates a plan in ~/.if0/.environments/$NAME/dash1.plan`
+func MakePlan(envName string) error {
+	ctx := context.Background()
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		fmt.Println("Error: ContainerClient - ", err)
+		return err
+	}
+
+	status, err := dockerClient.ImagePull(ctx, dash1Image, types.ImagePullOptions{})
+	if err != nil {
+		fmt.Println("Error: ImagePull - ", err)
+		return err
+	}
+	_, _ = io.Copy(os.Stdout, status)
+
+	//binding mounts
+	mounts := []mount.Mount{
+		{Type: mount.TypeBind,
+			Source: filepath.Join(common.RootPath, ".if0", ".environments", "if0-config"),
+			Target: "/root/.if0/.environments/zero"},
+		{Type: mount.TypeBind,
+			Source: filepath.Join(common.RootPath, ".gitconfig"),
+			Target: "/root/.gitconfig"}}
+	hostConfig := &container.HostConfig{Mounts: mounts}
+
+	resp, err := dockerClient.ContainerCreate(ctx, &container.Config{
+		Image: dash1Image,
+		Cmd:   []string{"make", "plan"},
+		Tty:   true,
+	}, hostConfig, nil, "dash1-"+envName)
+	if err != nil {
+		fmt.Println("Error: ContainerCreate - ", err)
+		return err
+	}
+
+	if err := dockerClient.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		fmt.Println("Error: ContainerStart - ", err)
+		return err
+	}
+
+	statusCh, errCh := dockerClient.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			fmt.Println("Error: ContainerWait - ", err)
+			return err
+		}
+	case <-statusCh:
+	}
+
+	out, err := dockerClient.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		fmt.Println("Error: ContainerLogs - ", err)
+		return err
+	}
+	_, _ = stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	err = dockerClient.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{})
+	if err != nil {
+		fmt.Println("Error: ContainerRemove - ", err)
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,21 @@ module if0
 go 1.14
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/djherbis/times v1.2.0
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/go-git/go-git/v5 v5.0.0
+	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.5.0
+	github.com/sirupsen/logrus v1.5.0 // indirect
 	github.com/spf13/cast v1.3.1
 	github.com/spf13/cobra v0.0.7
 	github.com/spf13/viper v1.6.2
@@ -15,5 +25,8 @@ require (
 	github.com/stretchr/testify v1.5.1
 	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
-	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
+	golang.org/x/sys v0.0.0-20200513112337-417ce2331b5c // indirect
+	gotest.tools v2.2.0+incompatible // indirect
 )
+
+replace github.com/docker/docker => github.com/docker/engine v17.12.0-ce-rc1.0.20190717161051-705d9623b7c1+incompatible
